d80099b89feaa0f1072a8520b2eaea92ec5a222f
robinparadise <robinchogiles@gmail.com>
2015-04-14 20:27:18 +0100

    Move poject to Klondike folder && Add empty class MoveController / test

--- klondike/klondike/StartGameControllerTest.java
+++ klondike/klondike/StartGameControllerTest.java
@@ -1,15 +1,10 @@
 package klondike;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.Stack;
 
-import org.junit.Before;
-import org.junit.Test;
-
 public class StartGameControllerTest {
-	
+
 	private StartGameController startGameController;
 
 	@Before
@@ -21,38 +16,41 @@ public class StartGameControllerTest {
 	public void sizeWasteTest() {
 		assertEquals(startGameController.sizeWaste(), 0);
 	}
-	
+
 	@Test
 	public void sizeFoundationsTest() {
-		ArrayList<Integer> sizeFoundations = startGameController.sizeFoundations();
+		ArrayList<Integer> sizeFoundations = startGameController
+				.sizeFoundations();
 		assertEquals(4, sizeFoundations.size());
 		for (int sizeFoundation : startGameController.sizeFoundations()) {
 			assertEquals(0, sizeFoundation);
 		}
 	}
-	
+
 	@Test
 	public void sizeDeckTest() {
 		assertEquals(startGameController.sizeDesk(), 24);
 	}
-	
+
 	@Test
 	public void sizeTableausTest() {
-		ArrayList<Integer> sizeTableaus = startGameController.sizeCoveredCardsTableaus();
-		ArrayList<Stack<Card>> uncoveredCardsStackTableaus = startGameController.uncoveredCardsStackTableau();
-		
+		ArrayList<Integer> sizeTableaus = startGameController
+				.sizeCoveredCardsTableaus();
+		ArrayList<Stack<Card>> uncoveredCardsStackTableaus = startGameController
+				.uncoveredCardsStackTableau();
+
 		assertEquals(7, sizeTableaus.size());
 		assertEquals(7, uncoveredCardsStackTableaus.size());
-		
+
 		for (int i = 0; i < sizeTableaus.size(); i++) {
 			assertEquals(new Integer(i + 1), sizeTableaus.get(i));
 		}
-		
+
 		for (Stack<Card> uncoveredCardsStack : uncoveredCardsStackTableaus) {
 			assertEquals(1, uncoveredCardsStack.size());
 			assertTrue(uncoveredCardsStack.peek().uncovered());
 		}
-		
+
 	}
 
 }
